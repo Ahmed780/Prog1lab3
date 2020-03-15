2ba88e330d69eae28b9c0144c478248cb6641ef8
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-03-15 15:22:51 -0700

    Update Hitting (1).java

--- Textbook/Hitting (1).java
+++ Textbook/Hitting (1).java
@@ -1,30 +1,28 @@
 import java.util.*;
-
 public class Hitting {
-  public static void main(String[] args) {
+ 
+	public static void main(String[] args) {
 
-    double hit;
-    double hit2;
-    double avg;
+    	double hit;
+    	double hit2;
+    	double avg;
 
-    Scanner temp = new Scanner(System.in);
-
-    System.out.println("\nPlease enter number of hits/runs");
-    hit = temp.nextDouble();
+    	Scanner temp = new Scanner(System.in);
+    	System.out.println("\nPlease enter number of hits/runs");
+    	hit = temp.nextDouble();
     
-    System.out.println("\nPlease enter number of at-bats/balls faced");
-    hit2 = temp.nextDouble();
+    	System.out.println("\nPlease enter number of at-bats/balls faced");
+    	hit2 = temp.nextDouble();
 
-    avg = hit * 100 / hit2 ;
+    	avg = hit * 100 / hit2 ;
+    	System.out.println("your batting average is "+ avg);
 
-    System.out.println("your batting average is "+ avg);
-
-    if(avg > 300) {
-    System.out.println("\nyou are eligible");
-		}
-    else if(avg < 300 ) {
-    System.out.println("\nyou are not eligible");
-    }
+    	if(avg > 300) {
+    	System.out.println("\nyou are eligible");
+	}
+    	else if(avg < 300 ) {
+    	System.out.println("\nyou are not eligible");
+    	}
 
   }
 }
