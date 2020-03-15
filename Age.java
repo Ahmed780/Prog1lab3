26fa87cb8439764beb6a6980230ef6a2630d42b0
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-03-15 16:02:07 -0700

    Add files via upload

--- Age.java
+++ Age.java
@@ -1,23 +1,33 @@
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.Scanner;
+import java.util.*;
+import java.util.Calendar;
 
-	public class Main {
- 
-    public static void main(String[] args) {
-        System.out.print("Please enter date of birth in YYYY-MM-DD: ");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        scanner.close();
- 
-        LocalDate dob = LocalDate.parse(input);
-        System.out.println("Age is:" + getAge(dob));
+public class Lab3Question5{
+    
+    public static int today(LocalDate x){
+        LocalDate date=LocalDate.now();
+        return Period.between(x,date).getYears();
     }
- 
-    // Returns age given the date of birth
-    public static int getAge(LocalDate dob) {
-        LocalDate curDate = LocalDate.now();
-        return Period.between(dob, curDate).getYears();
-    }
- 
+    public static void main(String[]args){
+        System.out.println("Enter birthday in yyyy-mm-dd");
+        System.out.println("Remember to add 0 in front mm 09 for september");
+        Scanner z=new Scanner(System.in);
+        String birthday= z.nextLine();
+        
+        LocalDate x=LocalDate.parse(birthday);
+        System.out.println("You are " + today(x));
+        
+            LocalDate today=LocalDate.now();
+            LocalDate Bday=x;
+            LocalDate nextBday=x.withYear(today.getYear());
+            
+            if(nextBday.isBefore(today)||nextBday.isEqual(today)){
+                nextBday=nextBday.plusYears(1);
+            }
+            Period p=Period.between(today,nextBday);
+            long pe=Period.between(today,nextBday).getMonths();
+            long pe2=Period.between(today,nextBday).getDays();
+            System.out.println("Your birthday is in "+p.getMonths()+" months and "+p.getDays()+" days");
+        }
 }
