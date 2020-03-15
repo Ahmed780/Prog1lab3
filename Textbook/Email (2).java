5eb25a558e4f3d5cdaa7099744ccbbd795654c20
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-03-15 15:20:00 -0700

    Update Email (2).java

--- Textbook/Email (2).java
+++ Textbook/Email (2).java
@@ -1,10 +1,9 @@
 import java.util.*;
-
 public class EmailAddress {
-  public static void main(String[] args) {
+    
+    public static void main(String[] args) {
 
     Scanner input = new Scanner(System.in);
-
     System.out.print(" Enter a word: " );
     String word = input.nextLine();
 
