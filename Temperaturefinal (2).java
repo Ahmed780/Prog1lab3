fa7c4e173bead3ec207d048d37e078b39a61e21d
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-03-15 15:18:52 -0700

    Update Temperaturefinal (2).java

--- Temperaturefinal (2).java
+++ Temperaturefinal (2).java
@@ -9,16 +9,15 @@ public class Temperaturefinal{
 	Scanner tp= new Scanner(System.in);
 	System.out.println("Type temperature in celsius");
 	// asks the user for temperature in C
-    temp1=tp.nextDouble();
+    	temp1=tp.nextDouble();
   
-    System.out.println( "\nTemperature is " + temp1 + " C.");
-    // prints the entered temperature in C
+    	System.out.println( "\nTemperature is " + temp1 + " C.");
+    	// prints the entered temperature in C
 	  
-	temp2 = (temp1 < 0) ? "cold":"hot"; 
-	  // if temp less than 0 then cold else if hot
-      System.out.println("\n" + temp2);
-	  // prints a message based on temperature
-     
+	temp2 = (temp1 < 0) ? "It is cold":"It ishot"; 
+	// if temp less than 0 then cold else if hot
+      	System.out.println("\n" + temp2);
+	// prints a message based on temperature     
   }
   
 }
